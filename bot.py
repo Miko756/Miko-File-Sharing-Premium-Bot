387c7160526edd41c57cba0ab217bcfc5a259585
Miko756 <164617880+Miko756@users.noreply.github.com>
2024-06-15 22:40:07 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -89,7 +89,7 @@ class Bot(Client):
         #web-response
         app = web.AppRunner(await web_server())
         await app.setup()
-        bind_address = "8.8.8.8"
+        bind_address = "0.0.0.0"
         await web.TCPSite(app, bind_address, PORT).start()
 
     async def stop(self, *args):
